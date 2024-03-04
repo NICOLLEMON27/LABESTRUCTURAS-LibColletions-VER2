65f2404bb2a7f766660bb41534a7f57c59501845
NICOLLEMON27 <142424875+NICOLLEMON27@users.noreply.github.com>
2024-03-04 16:55:46 -0500

    Add files via upload

---
+++ solCollections/libCollections/pkgCollections/pkgLineal/pkgADT/clsADTLineal.cs
@@ -0,0 +1,43 @@
+﻿using pkgServices.pkgCollections.pkgLineal.pkgInterfaces;
+using System;
+
+namespace pkgServices.pkgCollections.pkgLineal.pkgADT
+{
+    public class clsADTLineal<T> : iADTLineal<T> where T : IComparable<T>
+    {
+        int length;
+        public bool opModify(int idx, T item)
+        {
+            throw new NotImplementedException();
+        }
+        public bool opRetrieve(int idx, T item)
+        {
+            throw new NotImplementedException();
+        }
+        public int opFind(T item)
+        {
+            throw new NotImplementedException();
+        }
+        public bool opExist(T item)
+        {
+            throw new NotImplementedException();
+        }
+        public int opGetLength()
+        {
+            throw new NotImplementedException();
+        }
+        public T[] opToArray()
+        {
+            throw new NotImplementedException();
+        }
+        public string opToString()
+        {
+            throw new NotImplementedException();
+        }
+        public bool opToItems(T array)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ solCollections/libCollections/pkgCollections/pkgLineal/pkgVector/clsVectorStack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using pkgServices.pkgCollections.pkgLineal.pkgADT;
+using pkgServices.pkgCollections.pkgLineal.pkgInterfaces;
+
+namespace pkgServices.pkgCollections.pkgLineal.pkgVector
+{
+    public class clsVectorStack<T> :clsADTVector<T>, iStack<T> where T : IComparable<T>
+    {
+        public bool opPeek(ref T prmItem)
+        {
+            throw new NotImplementedException();
+        }
+        public bool opPop(ref T prmItem)
+        {
+            throw new NotImplementedException();
+        }
+        public bool opPush(T prmItem)
+        {
+            throw new NotImplementedException();
+        }
+        public clsVectorStack(int Capacity) { }
+        public clsVectorStack() { }
+
+    }
+}
